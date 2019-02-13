df1c4143130e1a22f684aff9b214dbc1f0db896d
Geurt Sengers <geurtsengers@Geurts-MacBook.local>
2019-02-13 13:05:03 +0100

    Add GET todod/:id path

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const {mongoose} = require('./db/mongoose');
+const {ObjectID} = require('mongodb');
 const {Todo} = require('./models/todo');
 const {User} = require('./models/user'); 
 
@@ -28,6 +29,26 @@ app.get('/todos', (req, res) => {
 	}, (e) => {
 		res.status(400).send(e);
 	});
+});
+
+app.get('/todos/:id', (req, res) => {
+	var id = req.params.id;
+
+	// validate id
+	if (!ObjectID.isValid(id)) {
+		return res.status(404).send('Invalid ID');
+	}
+
+	Todo.findById(id).then((todo) => {
+		if (!todo) {
+			return res.status(404).send('Todo not found.');
+		}
+		
+		res.send({todo}); // todo as property is more flexible 
+	}).catch((e) => {
+		res.status(400).send('Error');
+	});
+
 });
 
 app.listen(3000, () => {
