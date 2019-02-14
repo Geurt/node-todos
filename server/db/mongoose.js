208c7e0d9ef1affe7fba0cb6f8cdbdd3d6c74449
Geurt Sengers <geurtsengers@Geurts-MacBook.local>
2019-02-14 13:33:27 +0100

    Setup app for heroku and atlas

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,14 @@
 const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp', {useNewUrlParser: true});
+
+// local
+// mongoose.connect('mongodb://localhost:27017/TodoApp', {useNewUrlParser: true});
+
+// mongodb atlas
+mongoose.connect('mongodb+srv://grsengers:helmut79@geurt-test-shgnm.mongodb.net/test?retryWrites=true', {useNewUrlParser: true});
+
+// mlab heroku
+// mongoose.connect(process.env.MONGODB_URI);
 
 module.exports = {mongoose};
