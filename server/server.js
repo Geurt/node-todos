9e48efb0b4c1fa08d5ba372f7400faa51f48059c
Geurt Sengers <geurtsengers@Geurts-MacBook.local>
2019-02-15 13:40:02 +0100

    Add PATCH /todos/:id route

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const _ = require('lodash');
 
 const {mongoose} = require('./db/mongoose');
 const {ObjectID} = require('mongodb');
@@ -69,6 +70,34 @@ app.get('/todos/:id', (req, res) => {
 	});
 
 });
+
+app.patch('/todos/:id', (req, res) => {
+	var id = req.params.id;
+	var body = _.pick(req.body, ['text', 'completed']);
+
+	if (!ObjectID.isValid(id)) {
+		return res.status(404).send('Invalid ID');
+	}
+
+	if (_.isBoolean(body.completed) && body.completed) {
+		body.completedAt = new Date().getTime();
+	} else {
+		body.completed = false;
+		body.completedAt = null;
+	}
+
+	Todo.findByIdAndUpdate(id, {$set: body}, {new: true}).then((todo) => {
+		if (!todo) {
+			return res.status(404).send();
+		}
+
+
+		res.send({todo});
+	}).catch((e) => {
+		res.status(404).send();
+	});
+});
+
 
 app.listen(port, () => {
 	console.log(`Started on port ${port}.`);
